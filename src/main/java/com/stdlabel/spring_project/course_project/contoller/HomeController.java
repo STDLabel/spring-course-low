9b79f4f84b8125a62f5dee22cb458a5c4d7de190
somenonename <aspire5745G!>
2019-01-20 18:54:34 +0200

    [step_2] hello world controller

--- src/main/java/com/stdlabel/spring_project/course_project/contoller/HomeController.java
+++ src/main/java/com/stdlabel/spring_project/course_project/contoller/HomeController.java
@@ -4,18 +4,15 @@ package com.stdlabel.spring_project.course_project.contoller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloWorldController {
+public class HomeController {
 
 
     @GetMapping(value = "/")
     public String helloWorld(Model model) {
         model.addAttribute("text", "Hello world");
-        return "hello";
+        return "home";
     }
 
 
