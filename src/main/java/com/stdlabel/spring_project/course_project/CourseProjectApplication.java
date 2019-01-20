51b20d750792e1f1be73013905512048ec1ca196
Titov, Andrei <andrei.titov@hireright.com>
2019-01-20 15:33:13 +0200

    [step_1] alpha version

---
+++ src/main/java/com/stdlabel/spring_project/course_project/CourseProjectApplication.java
@@ -0,0 +1,14 @@
+package com.stdlabel.spring_project.course_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProjectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/stdlabel/spring_project/course_project/CourseProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.stdlabel.spring_project.course_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourseProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
