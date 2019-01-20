1737829d691d522c0a648f9e5cbf057b7ec60203
somenonename <aspire5745G!>
2019-01-20 18:52:56 +0200

    [step_2] hello world controller

--- src/main/java/com/stdlabel/spring_project/course_project/contoller/HelloWorldController.java
+++ src/main/java/com/stdlabel/spring_project/course_project/contoller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.stdlabel.spring_project.course_project.contoller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,10 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloWorldController {
 
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
-    public String helloWorld() {
-        return "Hello world";
+    @GetMapping(value = "/")
+    public String helloWorld(Model model) {
+        model.addAttribute("text", "Hello world");
+        return "hello";
     }
 
 
